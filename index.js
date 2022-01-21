93b5e8699292cb169e16b031e4359d5f682d0f53
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-21 14:51:54 -0400

    retornando o metodo

--- index.js
+++ index.js
@@ -5,22 +5,21 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    #saldo;
+    _saldo = 0;
+    //#saldo;
 
     sacar(valor) {
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor;
-
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor) {
-        if (valor > 0) {
-            this.#saldo += valor;
-        }
+        if (valor <= 0) return;
+        this._saldo += valor;
     }
 }
-
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
@@ -32,10 +31,10 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(300);
+contaCorrenteRicardo.sacar(100);
+contaCorrenteRicardo.sacar(221);
 
 console.log(contaCorrenteRicardo);
