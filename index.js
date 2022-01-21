422fdac91846a17d0fb4db7c252a641625155810
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-21 14:34:28 -0400

    criando classes e atributos privados

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
-
 }
+
+class ContaCorrente {
+    agencia;
+    #saldo;
+
+    sacar(valor) {
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this.#saldo += valor;
+        }
+    }
+}
+
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456;
-
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
-console.log(cliente1, cliente2);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo);
